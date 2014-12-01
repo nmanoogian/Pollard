b6d85da7e015c6d4a2e272ec6411f1dbaeb61d46
alkaline9500 <zimmoz3@verizon.net>
2014-12-01 09:45:55 -0500

    Adds "a" as a optional value for both the function and the command-line arg.

--- pollard.py
+++ pollard.py
@@ -1,9 +1,12 @@
 from fractions import gcd
 import argparse
 
-def pollard(n, B, a):
+def pollard(n, B, a=2):
+    """
+    Runs the Pollard Factorization Algorithm with n, B, and optional a
+    """
     for j in range(2, B):
-        print(a)
+        print("\t{a}".format(a=a))
         a = pow(a, j) % n
     d = gcd(a-1, n)
 
@@ -17,13 +20,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Pollard Factorization Algorithm.')
     parser.add_argument('n', type=int, help='number to test')
     parser.add_argument('B', type=int, help='a large number to use for factorization')
-    parser.add_argument('a', type=int, help='a number to use for exponentiation')
+    parser.add_argument('-a', metavar="a", type=int, help='a number to use for exponentiation')
     args = parser.parse_args()
 
     n = args.n
     B = args.B
 
-    d = pollard(n, B, args.a)
+    d = pollard(n, B, args.a or 2)
     if d:
         print("{d} is a factor of {n}".format(d=d, n=n))
     else:

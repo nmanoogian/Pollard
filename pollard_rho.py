9e974371b07e41fb82298a3148ff41715871b552
alkaline9500 <zimmoz3@verizon.net>
2014-12-03 09:43:12 -0500

    Adds x^2 - 1 implementation to show lambda use.

--- pollard_rho.py
+++ pollard_rho.py
@@ -15,6 +15,7 @@ def pollard_rho(n, g=lambda x, n: (x**2 + 1) % n):
         y = g(g(y, n), n)
         d = gcd(abs(x-y), n)
         print("x={x} y={y} d={d}".format(x=str(x).ljust(n_length), y=str(y).ljust(n_length), d=str(d).ljust(n_length)))
+        iterations += 1
     if d == n:
         return None
     else:
@@ -28,8 +29,15 @@ if __name__ == "__main__":
 
     n = args.n
 
-    print("Pollard-Rho:")
+    print("Pollard-Rho: g(x) = x^2 + 1")
     d = pollard_rho(n)
+    if d:
+        print("{d} is a factor of {n}".format(d=d, n=n))
+    else:
+        print("no factor of {n} is found".format(n=n))
+
+    print("\nPollard-Rho: g(x) = x^2 - 1")
+    d = pollard_rho(n, g=lambda x, n: (x**2 - 1) % n)
     if d:
         print("{d} is a factor of {n}".format(d=d, n=n))
     else:

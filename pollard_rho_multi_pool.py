6376bbb022fea0672ad898f7b342aabc1a9261e2
Nic Manoogian <zimmoz3@verizon.net>
2014-12-07 15:43:24 -0500

    Adds thread-pool implementation.

---
+++ pollard_rho_multi_pool.py
@@ -0,0 +1,41 @@
+from fractions import gcd
+import argparse
+import multiprocessing as mp
+
+class Number():
+    """
+    Represents a number and a single factor
+    """
+    def __init__(self, value, factor):
+        self.value = value
+        self.factor = factor
+
+    def __repr__(self):
+        if self.factor == self.value:
+            return "{v} is prime".format(v=self.value)
+        return "{v}={f}*x".format(v=self.value, f=self.factor)
+
+def pollard_rho(n, g=lambda x, n: (x**2 + 1) % n):
+    """
+    Runs the Pollard-Rho Algorithm
+    The default g function: g(x, n) = (x^2 + 1) mod n
+    """
+    n_length = len(str(n)) + 1
+    x = 2
+    y = 2
+    d = 1
+    while d == 1:
+        x = g(x, n)
+        y = g(g(y, n), n)
+        d = gcd(abs(x-y), n)
+    return Number(n, d)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Pollard-Rho Primality Testing')
+    parser.add_argument('n', type=int, help='number to which to test up')
+    args = parser.parse_args()
+    n = args.n
+
+    pool = mp.Pool()
+    for x in pool.map(pollard_rho, range(2, n)):
+        print(x)
